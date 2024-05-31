0c41c7bf7586095024ea82d3444e54b0f5dc393a
Federico Trani <federicotrani@gmail.com>
2024-05-30 21:45:14 -0300

    Ajustes pantalla de login

--- MauiApp1/Views/LoginPage.xaml.cs
+++ MauiApp1/Views/LoginPage.xaml.cs
@@ -6,4 +6,15 @@ public partial class LoginPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void btnLoginIngresar_Clicked(object sender, EventArgs e)
+    {
+		if(entLoginEmail.Text=="ites" && entLoginPassword.Text == "1234")
+		{
+			Navigation.PushAsync(new PrincipalPage());
+		}else
+		{
+			DisplayAlert("Atención","las credenciales ingresadas no son válidas","OK");
+		}
+    }
 }
